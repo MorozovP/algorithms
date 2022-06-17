5d34a7ec6964dd1e84a4cbcf0e753254e0af77e2
Павел Морозов <filimon.doors@gmail.com>
2022-06-17 12:27:20 +0300

    sprint 11

---
+++ Sprint 11 Base algo/a_closest_zero.py
@@ -0,0 +1,30 @@
+# Спринт 11 A. Ближайший ноль
+
+from typing import List
+
+
+def closest_zero(arr: List[int]) -> List[int]:
+    arr.reverse()
+    zero_index: int = -1
+    for index, value in enumerate(arr):
+        if value == 0:
+            zero_index = index
+        elif zero_index != -1 and (index - zero_index < value or value == -1):
+            arr[index] = index - zero_index
+    return arr
+
+
+def read_input() -> List[int]:
+    _ = input()
+    arr: list = [0 if x == '0' else -1 for x in input().split()]
+    return arr
+
+
+def main() -> None:
+    array: list = read_input()
+    result: list = closest_zero(closest_zero(array))
+    print(*result)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Sprint 11 Base algo/b_typing_trainer.py
@@ -0,0 +1,37 @@
+# Спринт 11 B. Ловкость рук
+
+from typing import Tuple
+
+NUMBER_OF_ROWS = 4
+NUMBER_OF_PLAYERS = 2
+
+
+def typing_trainer(k: int, digits: str) -> int:
+    nums_dict: dict = {}
+    for num in digits:
+        nums_dict[num] = nums_dict.get(num, 0) + 1
+    result: int = 0
+    for num in nums_dict.values():
+        if num <= k * NUMBER_OF_PLAYERS:
+            result += 1
+    return result
+
+
+def read_input() -> Tuple[int, str]:
+    digits: str = ''
+    k: int = int(input())
+    for _ in range(NUMBER_OF_ROWS):
+        for c in input():
+            if c.isdigit():
+                digits += c
+    return k, digits
+
+
+def main() -> None:
+    k, digits = read_input()
+    result = typing_trainer(k, digits)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
